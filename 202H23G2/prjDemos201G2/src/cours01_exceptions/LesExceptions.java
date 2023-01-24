acdd77880524678e390c335692e8beb90a4447cf
Robert Aubé <robert.aube@cegeplimoilou.ca>
2023-01-24 17:45:25 -0500

    G2Cours2

--- 202H23G2/prjDemos201G2/src/cours01_exceptions/LesExceptions.java
+++ 202H23G2/prjDemos201G2/src/cours01_exceptions/LesExceptions.java
@@ -1,12 +1,53 @@
 package cours01_exceptions;
 
+import java.io.IOException;
+
 public class LesExceptions {
     public LesExceptions() {
+        LaArithmeticException();
+    }
+    public void LaArithmeticException() {
+        try {
+            lancerUneException();
+        } catch (ArithmeticException exception) {
+        }
+
+        System.out.println("Fin LaArithmeticException!");
     }
 
-    private void lancerUneException()  {
+    public void LaIOException() {
+        try {
+            lancerUneIOExceptionV2();
+        } catch (IOException e) {
+            System.out.println("Erreur!");
+        }
+
+        System.out.println("Fin LaIOException!");
+    }
+
+    private void lancerUneExceptionV1() {
+        try {
+            throw new ArithmeticException("Test exception");
+        } catch (ArithmeticException exception) {
+            System.out.println("Exception traité");
+        }
+    }
+
+    private void lancerUneException() {
         throw new ArithmeticException("Test exception");
     }
+    private void lancerUneIOException() {
+        try {
+            throw new IOException("Test IOException");
+        } catch (IOException e) {
+            System.out.println("IOException est déclenché");
+        }
+    }
+
+    private void lancerUneIOExceptionV2() throws IOException { //IOException est propagé
+            throw new IOException("Test IOException");
+    }
+
 
     public static void main(String[] args) {
         new LesExceptions();
--- 202H23G2/prjDemos201G2/src/cours01_fichierEtTexteSplit/FichierTexte.java
+++ 202H23G2/prjDemos201G2/src/cours01_fichierEtTexteSplit/FichierTexte.java
@@ -1,29 +1,58 @@
 package cours01_fichierEtTexteSplit;
 
+import java.io.*;
 import java.util.ArrayList;
 
 public class FichierTexte {
+    private static char fSep = File.separatorChar;
+    private static String path = System.getProperty("user.dir") + fSep + "src" + fSep + "fichiers" + fSep;
+
     private ArrayList<String> listeExpressions;
 
     public FichierTexte() {
+        System.out.println(path);
         listeExpressions = new ArrayList<>();
-        lireFichier("expressionsIn.txt");
+        lireFichier(path + "expressions.txt");
         afficher();
         trier();
-        ecrireFichier("expressionsOut.txt");
+        ecrireFichier(path + "expressionsOut.txt");
     }
 
     private void trier() {
     }
 
     private void ecrireFichier(String nomFichier) {
+        try {
+            PrintWriter sortie = new PrintWriter(new FileWriter(nomFichier)); //ouverture de fichier
+            for (String str : listeExpressions) {
+                sortie.println(str); //écriture
+            }
+            sortie.flush(); // vider les buffer et écrire dans le fichier immédatement (à faire au besoin)
+            sortie.close(); //fermeture du fichier
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     private void lireFichier(String nomFichier) {
+        String ligne;
+        try {
+            BufferedReader entree = new BufferedReader(new FileReader(nomFichier)); //ouverture de fichier
+            ligne = entree.readLine(); // lire une ligne
+            while (ligne != null) {
+                listeExpressions.add(ligne);
+                ligne = entree.readLine(); // lire une ligne
+            }
+            entree.close(); //fermeture du fichier
+        } catch (FileNotFoundException e) {
+            System.out.println("Fichier non trouvé : " + nomFichier);
+        } catch (IOException e) {
+            System.out.println("Problème de lecture");
+        }
     }
-    
+
     private void afficher() {
-        for (String str: listeExpressions) {
+        for (String str : listeExpressions) {
             System.out.println(str);
         }
     }
