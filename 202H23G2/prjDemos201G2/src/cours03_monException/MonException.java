e39496246d583b31e3346ad4902280b6c7a7e19f
Robert Aubé <robert.aube@cegeplimoilou.ca>
2023-02-01 02:32:26 -0500

    updateNoVours V2

---
+++ 202H23G2/prjDemos201G2/src/cours03_monException/MonException.java
@@ -0,0 +1,10 @@
+package cours03_monException;
+
+public class MonException extends Exception {
+    public MonException() {
+        super();
+    }
+    public MonException(String message) {
+        super(message);
+    }
+}
