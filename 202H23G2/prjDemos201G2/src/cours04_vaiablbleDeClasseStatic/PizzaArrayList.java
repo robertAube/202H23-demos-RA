e50688eed12ae03843f0b3cdf70ef75d6eb82352
Robert <robert.aube@cegeplimoilou.ca>
2023-02-03 11:48:07 -0500

    G2Cours4

---
+++ 202H23G2/prjDemos201G2/src/cours04_vaiablbleDeClasseStatic/PizzaArrayList.java
@@ -0,0 +1,48 @@
+package cours01_sortInterfaceComparable;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+
+/**
+ * @author Robert Aubé
+ * @version 1.0 (Date de création: 2022-11-29)
+ */
+public class PizzaArrayList {
+    private ArrayList<Pizza> listPizza;
+
+    public PizzaArrayList() {
+        this.listPizza = new ArrayList<Pizza>();
+    }
+
+    public void ajouterPizza(Pizza p) {
+        listPizza.add(p);
+    }
+
+    public void enleverPizza(int i) {
+        listPizza.remove(i);
+    }
+
+    public void enleverPizza(Pizza p) {
+        listPizza.remove(p);
+    }
+
+    public void avancerToutesLesPizzas() {
+        for (int i = 0; i < listPizza.size(); i++) {
+            listPizza.get(i).avancerX();
+        }
+    }
+
+    public void trier() {
+        listPizza.sort(Comparator.naturalOrder());
+    }
+    public void trierInverser() {
+        listPizza.sort(Comparator.reverseOrder());
+    }
+
+    @Override
+    public String toString() {
+        return "PizzaArrayList{" +
+                "listPizza=" + listPizza +
+                '}';
+    }
+}
---
+++ 202H23G2/prjDemos201G2/src/cours04_vaiablbleDeClasseStatic/PizzaStaticTest.java
@@ -0,0 +1,2 @@
+package cours04_vaiablbleDeClasseStatic;public class PizzaStaticTest {
+}
