b18a9fa215418bbea6541c6c1897deae0c4b5aca
Robert Aubé <robert.aube@cegeplimoilou.ca>
2023-02-27 10:10:20 -0500

    G1Cours11 prep

--- 202H23G1/prjDemos202G1/src/_utilitaire/Utilitaire.java
+++ 202H23G1/prjDemos202G1/src/_utilitaire/Utilitaire.java
@@ -4,11 +4,35 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.util.Random;
+import java.util.Scanner;
 
 public class Utilitaire {
     public static Random r = new Random();
     private static final char fSep = File.separatorChar;
     public static final String REPERTOIRE_PROJET_FICHIERS = System.getProperty("user.dir") + fSep + "fichiers" + fSep;
+
+    public static String lireString(String question) {
+        Scanner sc;
+        String reponse;
+
+        sc = new Scanner(System.in);
+
+        System.out.print(question);
+        reponse = sc.nextLine();
+
+        return reponse;
+    }
+
+
+    public static int lireInt(String question) {
+        String reponse;
+        int reponseInt;
+
+        reponse = lireString(question);
+        reponseInt = Integer.parseInt(reponse);
+
+        return reponseInt;
+    }
 
     public static int getRandomInRange(int min, int max) {
         return r.nextInt((max - min) + 1) + min;
